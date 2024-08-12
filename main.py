6e54bfde552f0fcaf4acc49f402f9a3a96648c9f
sui-cloud <pure199711@naver.com>
2024-08-12 16:45:55 +0900

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import electronic0806 as ec
 import pybasic as pb
-
+import project as pr
 
 #로그인 화면
 st.sidebar.title("로그인")
@@ -13,7 +13,7 @@ if user_pw=="1234" and user_id=="abc":
     st.sidebar.title("★수이의 포트폴리오★")
     #st.image("빨간머리앤셜리.jpg")
 
-    menu  =  st.sidebar.radio("메뉴선택", ["파이썬기초","탐색적 분석: 전기자동차", "머신러닝"],index=None)
+    menu  =  st.sidebar.radio("메뉴선택", ["파이썬기초","탐색적 분석: 전기자동차", "머신러닝","팀별프로젝트"],index=None)
     st.sidebar.write(menu)
 
     if menu=="탐색적 분석: 전기자동차":
@@ -22,5 +22,7 @@ if user_pw=="1234" and user_id=="abc":
         st.header("공사중")    
     elif menu=="파이썬기초":
         pb.basic()
+    elif menu=="팀별프로젝트":
+        pr.main_food()
            
 
